4c2928307d5642ddf7faa996e7d2a22ceba55569
fantabae08-ctrl <fantabae08@gmail.com>
2026-02-27 00:37:26 +0000

    somme

--- exogit.c
+++ exogit.c
@@ -1,10 +1,12 @@
 #include<stdio .h>
 int main (){
-    int a ,b;
+    int a ,b, some;
     printf("la premiers letter");
     scanf("%d",&a);
     printf(" la deuxieme");
     scanf("%d",&b);
+    somme=a+b;
+    printf("la somme est%d\n",a,b);
 
 return 0;
 }
